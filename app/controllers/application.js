dbb64dd3124d866f5caf53ace9ee5af91bb225d0
Jamie Wright <jamie@brilliantfantastic.com>
2014-10-24 22:51:42 -0400

    Call the server side OAuth url

---
+++ app/controllers/application.js
@@ -0,0 +1,10 @@
+import Ember from "ember";
+import ENV from '../config/environment';
+
+export default Ember.Controller.extend({
+  serverUrl: ENV.serverUrl,
+  harvestOAuthUrl: function() {
+    var origin = window.location.origin + "/protected";
+    return this.serverUrl + "/auth/harvest?origin=" + origin;
+  }.property()
+});
--- app/router.js
+++ app/router.js
@@ -5,6 +5,8 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route("protected");
+});
 
 export default Router;
