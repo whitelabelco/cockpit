b170614c95e4ff858709ca4c65e92c8e54de79c2
Jamie Wright <jamie@brilliantfantastic.com>
2014-11-02 22:41:24 -0500

    Starting the implementation of the harvest authenticator

--- app/authenticators/harvest.js
+++ app/authenticators/harvest.js
@@ -1,9 +1,26 @@
+import Ember from 'ember';
 import Base from 'simple-auth/authenticators/base';
 
 var HarvestAuthenticator = Base.extend({
   authenticate: function(params) {
-    console.log(params);
-    return new Ember.RSVP.Promise(function(resolve, reject) {});
+    var self = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      self.makeRequest("https://wlabel.harvestapp.com/account/who_am_i", "GET", { access_token: params.token }).then(function(response) {
+        console.log("RESPONSE: " + response);
+      }, function(xhr, status, error) {
+        console.log("ERROR: " + xhr.responseJSON);
+      });
+    });
+  },
+
+  makeRequest: function(url, type, data) {
+    return Ember.$.ajax({
+      url:         url,
+      type:        type,
+      data:        data,
+      dataType:    'json',
+      contentType: 'application/json'
+    });
   }
 });
 
